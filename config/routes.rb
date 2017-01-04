117a4a728769d635f1ae3e57132d7d53f10c2bc0
C238 <c238@C238s-iMac.local>
2017-01-04 10:53:57 +0530

    firstcommit for static pages with database app

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  root 'staticpages#home'
+
+  get 'staticpages/home'
+
+  get 'staticpages/help'
+
+  get 'staticpages/about'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
