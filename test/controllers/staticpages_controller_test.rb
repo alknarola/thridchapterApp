da8d30627f899132f8bad5bb2a3195d3958d66c1
C238 <c238@C238s-iMac.local>
2017-01-04 16:55:26 +0530

    testing

--- test/controllers/staticpages_controller_test.rb
+++ test/controllers/staticpages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticpagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get staticpages_home_url
     assert_response :success
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get staticpages_help_url
     assert_response :success
+     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get staticpages_about_url
     assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
 end
