8c9ad5c0d28a6647342ff64d779e2a5ea38a5c89
C238 <c238@C238s-iMac.local>
2017-01-05 16:47:29 +0530

    testing and user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Rails.application.routes.draw do
 
-  get 'staticpages/contact'
-
-  get 'staticpages/signup'
-
-  get 'staticpages/signin'
+  get 'user/new'
 
   root 'staticpages#home'
 
-  get 'staticpages/home'
+  get '/contact',to: 'staticpages#contact'
 
-  get 'staticpages/help'
+  get '/signup',to: 'staticpages#signup'
 
-  get 'staticpages/about'
+  get '/signin',to: 'staticpages#signin'
 
+  get '/home',to: 'staticpages#home'
+
+  get '/help',to: 'staticpages#help'
+
+  get '/about',to: 'staticpages#about'
+
+  get 'signup', to: 'user#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/staticpages_controller_test.rb
+++ test/controllers/staticpages_controller_test.rb
@@ -2,21 +2,39 @@ require 'test_helper'
 
 class StaticpagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get staticpages_home_url
+    get home_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get staticpages_help_url
+    get help_path
     assert_response :success
      assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
-    get staticpages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get signup" do
+    get signup_path
+    assert_response :success
+    assert_select "title", "SignUp | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get signin" do
+    get signin_path
+    assert_response :success
+    assert_select "title", "SignIn | Ruby on Rails Tutorial Sample App"
   end
 
 end
