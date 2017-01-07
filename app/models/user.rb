b459e25a0ae012765ef55e8f11c8813287fe68c8
C238 <c238@C238s-iMac.local>
2017-01-07 09:57:34 +0530

    Make a basic User model (including secure passwords)

---
+++ app/models/user.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class User < ApplicationRecord
+
+  before_save { email.downcase! }
+  validates :name, presence: true,length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true,length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },uniqueness: true
+
+  # validates(:name, presence: true)
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com foo@bar.com akl@narola.email ]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+      mixed_case_email = "Foo@ExAMPle.CoM"
+      @user.email = mixed_case_email
+      @user.save
+      assert_equal mixed_case_email.downcase, @user.reload.email
+    end
+
+end
